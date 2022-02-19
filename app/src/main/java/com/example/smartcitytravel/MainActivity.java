3f0d3f31b97453699787f4275e02b1fcddccbbd4
AbdulMuqeet <muqeeta96@gmail.com>
2022-02-19 10:39:31 +0500

    Create SplashScreen (Logo Screen)

--- app/src/main/java/com/example/smartcitytravel/MainActivity.java
+++ app/src/main/java/com/example/smartcitytravel/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.smartcitytravel;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.smartcitytravel.Login.LoginActivity;
+import com.example.smartcitytravel.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        moveToLoginActivity();
+    }
+
+    private void moveToLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
