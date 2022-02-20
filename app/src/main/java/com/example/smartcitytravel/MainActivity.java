363ef26cecf4e726d5507c93d2c07816d875972e
AbdulMuqeet <muqeeta96@gmail.com>
2022-02-20 11:39:36 +0500

    Create Login Activity Frontend

--- app/src/main/java/com/example/smartcitytravel/MainActivity.java
+++ app/src/main/java/com/example/smartcitytravel/MainActivity.java
@@ -6,16 +6,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.smartcitytravel.Login.LoginActivity;
-import com.example.smartcitytravel.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
         moveToLoginActivity();
     }
