d6383f42275f93258923ab2d9249ef87ed9a4e80
AbdulMuqeet <muqeeta96@gmail.com>
2022-03-07 13:42:18 +0500

    navigation drawer custom button applied remove toolbar

--- app/src/main/java/com/example/smartcitytravel/Home/HomeActivity.java
+++ app/src/main/java/com/example/smartcitytravel/Home/HomeActivity.java
@@ -3,9 +3,9 @@ package com.example.smartcitytravel.Home;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 
@@ -35,16 +35,10 @@ public class HomeActivity extends AppCompatActivity {
 
         preferenceHandler = new PreferenceHandler();
 
-        addToolbar();
         createHomeFragment(savedInstanceState);
+        navigationDrawerToggle();
         selectFragmentFromDrawer();
-        navigationDrawerButtonToggle();
 
-
-    }
-
-    public void addToolbar() {
-        setSupportActionBar(binding.toolbar.getRoot());
     }
 
     //change fragment base on selected activity
@@ -83,13 +77,18 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-    //navigation button animation
-    public void navigationDrawerButtonToggle() {
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, binding.getRoot(),
-                binding.toolbar.getRoot(), R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-
-        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+    //open and close navigation drawer
+    public void navigationDrawerToggle() {
+        binding.navigationDrawerImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    binding.drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    binding.drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
     }
 
     //create home fragment which is start screen in home activity
