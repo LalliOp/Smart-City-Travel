d537b2a8250529837b34f6d3c6b76c4a21ed5625
AbdulMuqeet <muqeeta96@gmail.com>
2022-03-15 22:06:10 +0500

    adjust destination activity path

--- app/src/main/java/com/example/smartcitytravel/Destination/DestinationActivity.java
+++ app/src/main/java/com/example/smartcitytravel/Destination/DestinationActivity.java
@@ -1,12 +1,14 @@
-package com.example.smartcitytravel;
+package com.example.smartcitytravel.Destination;
+
+import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.os.Bundle;
+import com.example.smartcitytravel.R;
 
-public class destinationactivity extends AppCompatActivity {
+public class DestinationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
